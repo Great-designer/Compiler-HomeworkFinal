cea8eefd45700a24caa0d64c2e34c99f7f26a50d
Great-designer <63183399+Great-designer@users.noreply.github.com>
2020-11-29 00:07:22 +0800

    Update Final.cpp

--- Final.cpp
+++ Final.cpp
@@ -614,21 +614,173 @@ int aaa,bbb;//自变量。n代表不区分的非终结符
 int f[8]={0,0,2,2,4,4,5,5};//栈顶
 int g[8]={0,5,1,1,3,3,0,5};//读入
 
+//未完成 
 //8a+b，共有6个非法状态 
 int error[7]={0,4,6,25,29,31,35};
 
-int translate()//符号翻译器，必须是上述7种符号才能进入翻译器 
+//未完成 
+int translate(char t)//符号翻译器，必须是上述7种符号才能进入翻译器 
 {
-	
+	switch(t)
+	{
+		case '\0'://字符串的结尾，书上为sharp，C语言为0 
+		{
+			return 0;
+		}
+		case '(':
+		{
+			return 1;
+		}
+		case '+':
+		{
+			return 2;
+		}
+		case '*':
+		{
+			return 3;
+		}
+		case ')': 
+		{
+			return 4;
+		}
+		case 'i':
+		{
+			return 5;
+		}
+		default://非法读入报错 
+		{
+			return -1;
+		}
+	}
 }
 
+//未完成 
+int expstack[2048];
+int expstacktop;//expstacktop总指向栈顶元素的下一个位置，即expstacktop-1才是栈顶元素 
+
+char cqc[2048];
+int cqctop;//cqctop总指向下一个要读的字符 
+
+int merging()//返回expstacktop应该减去的值 
+{
+	if(expstacktop==0)//栈为空 
+	{
+		return -1;
+	}
+	if(expstack[expstacktop-1]==5)//i，5
+	{
+		return 1;
+	}
+	if(expstacktop<3)//后面的规则要求栈里至少有3个字符，即012的位置必须有字符，expstacktop至少是3，避免下溢出错误 
+	{
+		return -1;
+	}
+	if(expstack[expstacktop-1]==6&&expstack[expstacktop-2]==2&&expstack[expstacktop-3]==6)//E+T，626
+	{
+		return 3;
+	}
+	if(expstack[expstacktop-1]==6&&expstack[expstacktop-2]==3&&expstack[expstacktop-3]==6)//T*F，636
+	{
+		return 3;
+	}
+	if(expstack[expstacktop-1]==4&&expstack[expstacktop-2]==6&&expstack[expstacktop-3]==1)//(E)，164
+	{
+		return 3;
+	}
+	return -1;
+}
 
 //未完成 
 //expr -> operator_expr| negate_expr| assign_expr| as_expr| call_expr| literal_expr| ident_expr| group_expr
-void expr()//表达式，8种可能，需要预读下一层分类 
+void expr()//表达式，8种可能，需要预读下一层分类。expr有可能调用expr，因此它涉及的全局变量需要改为申请 
 {
-	
+	fscanf(in,"%s",cqc);
+	cqctop=0;
+	expstacktop=0;
+	while(1)//减少用else，每个分支末尾尽可能continue或者break
+	{
+		if(cqctop==-1)//已经读过文件尾了
+		{
+			printf("E\n");//读入出错时输出E 
+			break;
+		}
+		bbb=translate(cqc[cqctop]);//读入不一定合法
+		if(bbb==-1)
+		{
+			printf("E\n");//读入出错时输出E 
+			break;
+		}
+		if(expstacktop!=0)//栈非空
+		{
+			aaa=expstack[expstacktop-1];//此时栈顶一定是合法字符的整数 
+			if(aaa==6)//栈顶是非终结符
+			{
+				if(expstacktop==1)
+				{
+					aaa=0;//表示前一个位置应该是sharp 
+				}
+				else
+				{
+					aaa=expstack[expstacktop-2];
+					if(aaa==6)//每一步非终结符都不能相邻，再前面一个应该是终结符才对 
+					{
+						printf("E\n");//违反规则时输出E  
+						break;
+					}
+				}
+			}
+			if(aaa==0&&bbb==0&&expstack[expstacktop-1]==6&&expstacktop==1)//整个程序顺利结束 
+			{
+				break;
+			}
+			int count=6*aaa+bbb;//查错误表 
+			int flag=0;//暂存
+			int i;
+			for(i=0;i<7;i++)
+			{
+				if(count==error[i])
+				{
+					printf("E\n");//无法判断优先级关系时输出E
+					flag=1;
+					break;//这个break没有跳出while
+				}
+			}
+			if(flag==1)
+			{
+				break;//这里break才能跳出while
+			}
+			if(f[aaa]>g[bbb])//仅大于的时候才规约，其他时候读入 
+			{
+				int rr=merging();
+				if(rr==-1)
+				{
+					printf("RE\n");//规约失败时输出RE 
+					break;
+				}
+				printf("R\n");//规约成功时输出R
+				expstacktop-=rr;
+				expstack[expstacktop]=6;//非终结符号 
+				expstacktop++;
+				continue;
+			}
+		}
+		expstack[expstacktop]=bbb;
+		expstacktop++;
+		if(cqc[cqctop]!='\0')//没读入文件尾字符的时候才要报读入 
+		{
+			printf("I%c\n",cqc[cqctop]);
+			cqctop++;
+		}
+		else
+		{
+			cqctop=-1;//已经读过文件尾了 
+		}
+	}
 }
+
+
+	
+
 
 // 未完成 
 // 造一条：比较运算符的运行结果是布尔类型，只能出现在 if 和 while 语句的条件表达式中。运算符的两侧必须是相同类型的数据 
